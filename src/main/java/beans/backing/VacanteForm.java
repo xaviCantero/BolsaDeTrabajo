cbf4cb089428013d2f4d02dcb47a72a8dc7b5ad7
Xavi <xavicanteros@gmail.com>
2022-01-28 17:31:54 -0300

    Se agrega el proyecto

---
+++ src/main/java/beans/backing/VacanteForm.java
@@ -0,0 +1,95 @@
+package beans.backing;
+
+import beans.model.Candidato;
+import beansHelper.ColoniaHelper;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIInput;
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import javax.faces.event.ValueChangeEvent;
+import javax.inject.Inject;
+import javax.inject.Named;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Named
+@RequestScoped
+public class VacanteForm {
+
+    @Inject
+    private Candidato candidato;
+
+    private boolean comentarioEnviado;
+
+    @Inject
+    private ColoniaHelper coloniaHelper;
+
+    Logger log = LogManager.getRootLogger();
+
+    public VacanteForm() {
+        log.info("Creando el objeto VacanteForm");
+    }
+
+    public void setCandidato(Candidato candidato) {
+        this.candidato = candidato;
+    }
+
+    public String enviar() {
+        if (this.candidato.getNombre().equals("Juan")) {
+            if (this.candidato.getApellido().equals("Perez")) {
+                String msg = "Gracias, pero Juan Perez ya trabaja con nosotros.";
+                FacesMessage facesMessage = new FacesMessage(FacesMessage.SEVERITY_ERROR, msg, msg);
+                FacesContext facesContext = FacesContext.getCurrentInstance();
+                String componentId = null; //este es un mensaje global
+                facesContext.addMessage(componentId, facesMessage);
+                return "index";
+            }
+            log.info("Entrando al caso de exito");
+            return "exito";
+        } else {
+            return "fallo";
+        }
+    }
+
+    public void codigoPostalListener(ValueChangeEvent valueChangeEvent) {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        UIViewRoot uiViewRoot = facesContext.getViewRoot();
+        int nuevoCodigoPostal = ((Long) valueChangeEvent.getNewValue()).intValue();
+
+        UIInput coloniaIdInputText = (UIInput) uiViewRoot.findComponent("vacanteForm:coloniaId");
+        int coloniaId = this.coloniaHelper.getColoniaIdPorCP(nuevoCodigoPostal);
+        coloniaIdInputText.setValue(coloniaId);
+        coloniaIdInputText.setSubmittedValue(coloniaId);
+
+        UIInput ciudadInputText = (UIInput) uiViewRoot.findComponent("vacanteForm:ciudad");
+        String nuevaCiudad = "Ciudad de Mexico";
+        ciudadInputText.setValue(nuevaCiudad);
+        ciudadInputText.setSubmittedValue(nuevaCiudad);
+
+        facesContext.renderResponse();
+
+    }
+
+    public void ocultarComentario(ActionEvent actionEvent) {
+        this.comentarioEnviado = !this.comentarioEnviado;
+    }
+
+    public boolean isComentarioEnviado() {
+        return comentarioEnviado;
+    }
+
+    public void setComentarioEnviado(boolean comentarioEnviado) {
+        this.comentarioEnviado = comentarioEnviado;
+    }
+
+    public ColoniaHelper getColoniaHelper() {
+        return coloniaHelper;
+    }
+
+    public void setColoniaHelper(ColoniaHelper coloniaHelper) {
+        this.coloniaHelper = coloniaHelper;
+    }
+
+}
